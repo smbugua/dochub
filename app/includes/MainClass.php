d296477a06f25ec99221f8d5d0754a2655dcdd5f
simon mbugua <simon.mbugua@dtone.com>
2021-02-17 23:50:36 +0300

    cleaned up and added the video iframe

---
+++ app/includes/MainClass.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dochub;
+class  MainClass{ 
+//connect to db
+function db_connect(){
+
+}    
+
+
+
+//create user
+
+function create_user(){
+
+}
+
+
+//login
+
+//set title
+function set_title($title){
+    echo $title;
+}
+
+
+}
